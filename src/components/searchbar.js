5970024166dc540911540e3dabea02a577e12d69
Pomeg21 <seotaekbyun@gmail.com>
2025-08-28 23:42:07 +0900

    Initial commit

---
+++ src/components/searchbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './searchbar.css';
+
+const SearchBar = () => {
+  return (
+<div>
+    <div className = 'input-wrapper'>
+        <input placeholder='Search...' />
+    </div>
+
+    <div>search results</div>
+
+</div>
+  )
+}
+
+export default SearchBar
